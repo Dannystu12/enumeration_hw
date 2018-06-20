4f0d849d25b0af9d46af02e2facf971a097ab535
Daniel Stewart <mgdog1@googlemail.com>
2018-06-20 13:35:47 +0100

    completed upper case and pangram

--- pangram_finder/pangram_finder.js
+++ pangram_finder/pangram_finder.js
@@ -1,9 +1,10 @@
 const PangramFinder = function (phrase) {
   this.alphabet = 'qwertyuiopasdfghjklzxcvbnm'.split('');
+  this.phrase = phrase;
 }
 
-PangramFinder.prototype.isPangram = function () {
-
+PangramFinder.prototype.isPangram = function (sentence) {
+  return this.alphabet.every(letter => this.phrase.toLowerCase().indexOf(letter) != -1);
 }
 
 module.exports = PangramFinder;
